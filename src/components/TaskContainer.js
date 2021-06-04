f09942b03ed0e6146d154d4fd67e3f3ccc4856a5
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-04 08:32:44 +0545

    Feat: Add tasks

--- src/components/TaskContainer.js
+++ src/components/TaskContainer.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from "react"
 import "./task_container.css"
 import TaskList from "./TaskList"
 import Loading from "./Loading"
+import { FaPlusSquare } from "react-icons/fa"
 
 const url = "http://localhost:8000/tasks"
 
 const TaskContainer = () => {
   const [loading, setLoading] = useState(true)
   const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState(false)
+  const [task, setTask] = useState("")
 
   const fetchData = async () => {
     try {
@@ -28,26 +31,54 @@ const TaskContainer = () => {
     fetchData()
   }, [])
 
+  const handleAdd = e => {
+    e.preventDefault()
+
+    const taskToAdd = {
+      id: Date.now(),
+      title: task,
+      completed: false,
+      status: "Pending",
+      priority: "Normal"
+    }
+    setNewTask(tasks.unshift(taskToAdd))
+    console.log("Task Added")
+
+    setNewTask(false)
+  }
+
   return (
     <section className="task-container">
-      <h1>
-        You've got{" "}
-        <span className="num-tasks">
-          {tasks.length} task{tasks.length > 1 ? "s" : ""}
-        </span>{" "}
-        today
-      </h1>
-      <form className="add-task" onSubmit={() => alert("Submitted")}>
-        <div className="form-control">
-          <input type="text" className="task" placeholder="Add Task Details" />
-          <button className="add-btn">Add</button>
-        </div>
-      </form>
+      <div className="title">
+        <h1>
+          You've got{" "}
+          <span className="num-tasks">
+            {tasks.length || "No"} task{tasks.length === 1 ? "" : "s"}
+          </span>{" "}
+          today
+        </h1>
+        <button className="add-btn" onClick={() => setNewTask(true)}>
+          <FaPlusSquare /> Add New
+        </button>
+      </div>
       {loading ? (
         <Loading />
       ) : (
         <div className="task-list">
           <h3>On Hold</h3>
+          {newTask ? (
+            <form className="add-task" onSubmit={handleAdd}>
+              <input
+                type="text"
+                className="task"
+                placeholder="Create a new task"
+                value={task}
+                onChange={({ target }) => setTask(target.value)}
+              />
+            </form>
+          ) : (
+            ""
+          )}
           {tasks.length
             ? tasks
                 .filter(({ completed }) => !completed)
